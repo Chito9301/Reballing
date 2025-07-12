dffd81f072c1fe20a77ad3ce3772341c40964edd
Chito9301 <85809089+Chito9301@users.noreply.github.com>
2025-07-11 22:40:38 -0400

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,1 +1,13 @@
+export default function Home() {
+  return (
+    <div>
+      <p>API KEY: {process.env.NEXT_PUBLIC_FIREBASE_API_KEY}</p>
+      <p>AUTH DOMAIN: {process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN}</p>
+      <p>PROJECT ID: {process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID}</p>
+      <p>STORAGE BUCKET: {process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET}</p>
+      <p>MESSAGING SENDER ID: {process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID}</p>
+      <p>APP ID: {process.env.NEXT_PUBLIC_FIREBASE_APP_ID}</p>
+    </div>
+  );
+}
 
