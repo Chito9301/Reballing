01ae99440472e4361204fb6dfe0ec28cf2908b78
Chito9301 <85809089+Chito9301@users.noreply.github.com>
2025-07-15 20:44:08 -0400

    Update firebase.ts

--- lib/firebase.ts
+++ lib/firebase.ts
@@ -23,6 +23,14 @@ const checkConfiguration = (): boolean => {
     'NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID',
     'NEXT_PUBLIC_FIREBASE_APP_ID'
   ];
+console.log("PRODUCTION Firebase ENV:", {
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+});
 
   return requiredKeys.every(key => {
     const value = process.env[key];
