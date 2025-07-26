a2352e7dac0ebfeb1e0d079c1703d9a5b8a10d44
Chito9301 <85809089+Chito9301@users.noreply.github.com>
2025-07-26 15:42:50 -0400

    Create index.js
    Fixed

---
+++ pages/index.js
@@ -0,0 +1,10 @@
+import MediaList from '../components/MediaList';
+
+export default function Home() {
+  return (
+    <main>
+      <h1>Bienvenido a la app</h1>
+      <MediaList />
+    </main>
+  );
+}
